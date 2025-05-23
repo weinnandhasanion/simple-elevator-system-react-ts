c023d5593228dd9c50d5e1dfe744abd9a44ac933
weinnandhasanion <weinnandhasanion@gmail.com>
2025-05-23 16:29:53 +0800

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 export default {
+  preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
